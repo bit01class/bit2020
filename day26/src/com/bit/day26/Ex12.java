1075ed6f08f362bccd3b350f0a857b571bef22cc
bit01class <bit01class@gmail.com>
2020-06-10 17:54:12 +0900

    game

--- day26/src/com/bit/day26/Ex12.java
+++ day26/src/com/bit/day26/Ex12.java
@@ -4,17 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Random;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-public class Ex12 extends Frame implements ActionListener {
+public class Ex12 extends Frame implements ActionListener,Runnable {
 	JButton[] btns;
+	Panel center;
+	int score;
 	
 	public Ex12() {
 		addWindowListener(new WindowAdapter() {
@@ -41,11 +48,31 @@ public class Ex12 extends Frame implements ActionListener {
 		east.add(begin);
 		east.add(exit);
 		
-		Panel center=new Panel();
+		center=new Panel();
 		center.setLayout(new GridLayout(4,4));
 		btns=new JButton[16];
-		for(int i=0; i<btns.length; i++){
-			btns[i]=new JButton();
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		for(int i=0; i<btns.length; i++){		
+			Image img=kit.createImage(1+i%4+".jpg");
+			Icon icon=new ImageIcon(img);
+			btns[i]=new JButton(icon);
+			if(i%4==0){
+				btns[i].addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						score++;
+					}
+				});
+			}else{
+				btns[i].addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						score-=2;
+					}
+				});
+			}
 			btns[i].setEnabled(false);
 			center.add(btns[i]);
 		}
@@ -58,18 +85,54 @@ public class Ex12 extends Frame implements ActionListener {
 		setLocation(200, 0);
 		setVisible(true);
 	}
+	
+	private void mix(){
+		Random ran=new Random();
+		JButton temp=null;
+		for(int i=0; i<1000; i++){
+			int su=ran.nextInt(15)+1;
+			temp=btns[0];
+			btns[0]=btns[su];
+			btns[su]=temp;
+		}
+		for(int i=0; i<btns.length; i++){
+			center.add(btns[i]);
+		}
+		revalidate();// 화면갱신
+	}
+	
+	private void play(){
+		for(int i=0; i<20; i++){
+			mix();
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("점수:"+score);
+		for(int i=0;i<btns.length; i++){
+			btns[i].setEnabled(false);
+		}
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		for(int i=0; i<btns.length; i++){
 			btns[i].setEnabled(true);
 		}
+		Thread thr=new Thread(this);
+		thr.start();
 	}
-	
 
 	public static void main(String[] args) {
 		Ex12 me=new Ex12();
 
+	}
+
+	@Override
+	public void run() {
+		play();
 	}
 
 }
