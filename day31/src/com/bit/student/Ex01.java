7806fa0f2fca0829d94970b63d1ae59b3d0b5b6b
bit01class <bit01class@gmail.com>
2020-06-23 17:22:52 +0900

    학생성적관리프로그램(ver 0.4.0)

--- day31/src/com/bit/student/Ex01.java
+++ day31/src/com/bit/student/Ex01.java
@@ -18,11 +18,17 @@ import javax.swing.border.EmptyBorder;
 
 import java.sql.*;
 import java.util.Properties;
+import javax.swing.JTextField;
 
 public class Ex01 extends JFrame {
 	JLabel[] table=new JLabel[50];
 
 	private JPanel contentPane;
+	private JTextField addNumtf;
+	private JTextField addNametf;
+	private JTextField addKortf;
+	private JTextField addEngtf;
+	private JTextField addMathtf;
 
 	/**
 	 * Launch the application.
@@ -139,11 +145,124 @@ public class Ex01 extends JFrame {
 			table[i]=new JLabel();
 			center.add(table[i]);
 		}
+		JPanel center2=new JPanel();
+		contentPane.add(center2, BorderLayout.CENTER);
+		GridBagLayout gbl_center2 = new GridBagLayout();
+		gbl_center2.columnWidths = new int[] {100, 0, 50};
+		gbl_center2.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_center2.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_center2.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		center2.setLayout(gbl_center2);
+		
+		JLabel lblNewLabel = new JLabel("\uC785\uB825\uD398\uC774\uC9C0");
+		lblNewLabel.setFont(new Font("±¼¸²", Font.BOLD, 18));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 2;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		center2.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel label_6 = new JLabel("\uD559\uBC88");
+		GridBagConstraints gbc_label_6 = new GridBagConstraints();
+		gbc_label_6.anchor = GridBagConstraints.EAST;
+		gbc_label_6.insets = new Insets(0, 0, 5, 5);
+		gbc_label_6.gridx = 0;
+		gbc_label_6.gridy = 1;
+		center2.add(label_6, gbc_label_6);
+		
+		addNumtf = new JTextField();
+		GridBagConstraints gbc_addNumtf = new GridBagConstraints();
+		gbc_addNumtf.insets = new Insets(0, 0, 5, 0);
+		gbc_addNumtf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addNumtf.gridx = 1;
+		gbc_addNumtf.gridy = 1;
+		center2.add(addNumtf, gbc_addNumtf);
+		addNumtf.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("\uC774\uB984");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 2;
+		center2.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		addNametf = new JTextField();
+		GridBagConstraints gbc_addNametf = new GridBagConstraints();
+		gbc_addNametf.insets = new Insets(0, 0, 5, 0);
+		gbc_addNametf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addNametf.gridx = 1;
+		gbc_addNametf.gridy = 2;
+		center2.add(addNametf, gbc_addNametf);
+		addNametf.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uAD6D\uC5B4");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 3;
+		center2.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		addKortf = new JTextField();
+		GridBagConstraints gbc_addKortf = new GridBagConstraints();
+		gbc_addKortf.insets = new Insets(0, 0, 5, 0);
+		gbc_addKortf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addKortf.gridx = 1;
+		gbc_addKortf.gridy = 3;
+		center2.add(addKortf, gbc_addKortf);
+		addKortf.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("\uC601\uC5B4");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 4;
+		center2.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		addEngtf = new JTextField();
+		GridBagConstraints gbc_addEngtf = new GridBagConstraints();
+		gbc_addEngtf.insets = new Insets(0, 0, 5, 0);
+		gbc_addEngtf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addEngtf.gridx = 1;
+		gbc_addEngtf.gridy = 4;
+		center2.add(addEngtf, gbc_addEngtf);
+		addEngtf.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("\uC218\uD559");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 0;
+		gbc_lblNewLabel_4.gridy = 5;
+		center2.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		addMathtf = new JTextField();
+		GridBagConstraints gbc_addMathtf = new GridBagConstraints();
+		gbc_addMathtf.insets = new Insets(0, 0, 5, 0);
+		gbc_addMathtf.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addMathtf.gridx = 1;
+		gbc_addMathtf.gridy = 5;
+		center2.add(addMathtf, gbc_addMathtf);
+		addMathtf.setColumns(10);
+		
+		JButton addBtn = new JButton("\uC785 \uB825");
+		GridBagConstraints gbc_addBtn = new GridBagConstraints();
+		gbc_addBtn.fill = GridBagConstraints.HORIZONTAL;
+		gbc_addBtn.gridx = 1;
+		gbc_addBtn.gridy = 6;
+		center2.add(addBtn, gbc_addBtn);
 		
 		btn1.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
+				center.setVisible(true);
+				center2.setVisible(false);
+				contentPane.add(center, BorderLayout.CENTER);
 				
 				String sql="select * from student01";
 				new oracle.jdbc.driver.OracleDriver();
@@ -177,6 +296,25 @@ public class Ex01 extends JFrame {
 						e1.printStackTrace();
 					}
 				}
+			}
+		});
+				
+		btn2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				center.setVisible(false);
+				center2.setVisible(true);
+				contentPane.add(center2, BorderLayout.CENTER);
+				
+			}
+		});
+	
+		addBtn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String sql="insert into student01 values (1,'test',100,100,100)";
+				
 			}
 		});
 	}
